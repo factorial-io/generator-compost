20345b8981622152c4a4fee39442d1f79d2ba3f1
Milan Matull <milan@factorial.io>
2019-09-17 12:01:27 +0200

    chore: code style (#5)

--- app/index.js
+++ app/index.js
@@ -1,118 +1,110 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
-var path = require ('path');
-var jf = require('jsonfile');
-var util = require('util');
-var _ = require('lodash');
+const yeoman = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const path = require("path");
+const jf = require("jsonfile");
+const util = require("util");
+const _ = require("lodash");
 
 jf.spaces = 2;
 
 module.exports = yeoman.generators.Base.extend({
-
-  initializing: function() {
-    this.pkg = require('../package.json');
+  initializing() {
+    this.pkg = require("../package.json");
   },
 
-  prompting: function() {
-    var done = this.async();
+  prompting() {
+    const done = this.async();
 
-    this.log(yosay(
-      'Welcome to the marvelous ' + chalk.red('Compost') + ' generator!'
-    ));
+    this.log(
+      yosay(`Welcome to the marvelous ${chalk.red("Compost")} generator!`)
+    );
 
-    var prompts = [
+    const prompts = [
       {
-        name: 'name',
-        message: 'What is your component\'s name?'
+        name: "name",
+        message: "What is your component's name?"
       },
       {
-        name: 'githubUsername',
-        message: 'GitHub username?',
-        default: 'factorial-io'
+        name: "githubUsername",
+        message: "GitHub username?",
+        default: "factorial-io"
       },
       {
-        type: 'confirm',
-        name: 'addTemplate',
-        message: 'Would you like to create a template?',
+        type: "confirm",
+        name: "addTemplate",
+        message: "Would you like to create a template?",
         default: true
       },
       {
-        type: 'list',
-        name: 'templateName',
-        message: 'What template language do you want to use?',
-        choices: [
-          'tpl.haml',
-          'tpl.php',
-          'tag'
-        ],
-        default: 'tpl.haml',
-        when: function (answers) {
+        type: "list",
+        name: "templateName",
+        message: "What template language do you want to use?",
+        choices: ["tpl.haml", "tpl.php", "tag"],
+        default: "tpl.haml",
+        when(answers) {
           return answers.addTemplate;
         }
       },
       {
-        type: 'confirm',
-        name: 'addStyles',
-        message: 'Would you like to create styles (SUIT CSS component)?',
+        type: "confirm",
+        name: "addStyles",
+        message: "Would you like to create styles (SUIT CSS component)?",
         default: true
       },
       {
-        type: 'confirm',
-        name: 'addScripts',
-        message: 'Would you like to create scripts (jQueryUI widget)?',
+        type: "confirm",
+        name: "addScripts",
+        message: "Would you like to create scripts (jQueryUI widget)?",
         default: true
       },
       {
-        type: 'list',
-        name: 'instantiate',
-        message: 'How should the widget be instantiated?',
-        choices: ['$(document).ready', 'Drupal.behaviors'],
-        default: '$(document).ready',
-        when: function (answers) {
+        type: "list",
+        name: "instantiate",
+        message: "How should the widget be instantiated?",
+        choices: ["$(document).ready", "Drupal.behaviors"],
+        default: "$(document).ready",
+        when(answers) {
           return answers.addScripts;
         }
       },
       {
-        type: 'list',
-        name: 'implement',
-        message: 'How do you want to resolve your dependencies?',
-        choices: [
-          'package.json',
-          'component.json',
-          'import'
-        ],
-        default: 'package.json'
+        type: "list",
+        name: "implement",
+        message: "How do you want to resolve your dependencies?",
+        choices: ["package.json", "component.json", "import"],
+        default: "package.json"
       }
     ];
 
-    this.prompt(prompts, function (props) {
-      this.props = {};
-      this.props = props;
-      done();
-    }.bind(this));
+    this.prompt(
+      prompts,
+      function(props) {
+        this.props = {};
+        this.props = props;
+        done();
+      }.bind(this)
+    );
   },
 
   writing: {
-
     /*
      * Adds a new local dependency to the project's component.json if not
      * already present.
      */
 
-    updateJson: function() {
-      var found = false;
+    updateJson() {
+      let found = false;
 
-      var file = this.props.implement;
-      var obj = jf.readFileSync(file, {throws: false});
+      const file = this.props.implement;
+      const obj = jf.readFileSync(file, { throws: false });
 
-      var isName = function(element) {
+      const isName = function(element) {
         return element === this.props.name;
       };
 
       if (obj) {
-        if (this.props.implement === 'package.json') {
+        if (this.props.implement === "package.json") {
           if (!obj.dependencies) {
             obj.dependencies = {};
             jf.writeFileSync(file, obj);
@@ -121,10 +113,12 @@ module.exports = yeoman.generators.Base.extend({
           found = _.some(obj.dependencies, isName.bind(this));
 
           if (!found) {
-            obj.dependencies[this.props.name] = 'file:./components_local/' + this.props.name;
+            obj.dependencies[
+              this.props.name
+            ] = `file:./components_local/${this.props.name}`;
             jf.writeFileSync(file, obj);
           }
-        } else if (this.props.implement === 'component.json') {
+        } else if (this.props.implement === "component.json") {
           if (!obj.locals) {
             obj.locals = {};
             jf.writeFileSync(file, obj);
@@ -140,36 +134,34 @@ module.exports = yeoman.generators.Base.extend({
       }
     },
 
-    changeDestinationRoot: function() {
-      var componentPath = null;
+    changeDestinationRoot() {
+      let componentPath = null;
 
-      if (this.props.implement === 'import') {
-        componentPath = '/source/source/components';
+      if (this.props.implement === "import") {
+        componentPath = "/source/source/components";
       } else {
-        componentPath = '/components_local';
+        componentPath = "/components_local";
       }
-      this.destinationRoot(path.join(
-        this.destinationRoot(),
-        componentPath,
-        '/' + this.props.name
-      ));
+      this.destinationRoot(
+        path.join(this.destinationRoot(), componentPath, `/${this.props.name}`)
+      );
     },
 
-    app: function() {
-      var options = {
+    app() {
+      const options = {
         addTemplate: this.props.templateName,
-        addStyles: 'css',
-        addScripts: 'js'
+        addStyles: "css",
+        addScripts: "js"
       };
 
-      var matchesProperty = function(n, key) {
-        return this.props[key] ? true : false;
+      const matchesProperty = function(n, key) {
+        return !!this.props[key];
       };
 
-      var copyTemplate = function(n) {
+      const copyTemplate = function(n) {
         this.fs.copyTpl(
-          this.templatePath('_component.' + n),
-          this.destinationPath(this.props.name + '.' + n),
+          this.templatePath(`_component.${n}`),
+          this.destinationPath(`${this.props.name}.${n}`),
           {
             name: this.props.name,
             instantiate: this.props.instantiate
@@ -181,72 +173,67 @@ module.exports = yeoman.generators.Base.extend({
         _.filter(options, matchesProperty.bind(this)),
         copyTemplate.bind(this)
       );
-
     },
 
-    packageFiles: function() {
-      var i;
-      var packages = [
-        '_component.json'
-      ];
+    packageFiles() {
+      let i;
+      const packages = ["_component.json"];
 
       for (i = 0; i < packages.length; i += 1) {
         this.fs.copyTpl(
           this.templatePath(packages[i]),
           this.destinationPath(packages[i].substr(1)),
-          {props: this.props}
+          { props: this.props }
         );
       }
 
       this.packageJson = {
-        'name': this.props.name,
-        'version': '0.0.0',
-        'repository': {
-          'type': 'git',
-          'url': 'https://github.com/' + this.props.githubUsername + '/' + this.props.name
+        name: this.props.name,
+        version: "0.0.0",
+        repository: {
+          type: "git",
+          url: `https://github.com/${this.props.githubUsername}/${this.props.name}`
         },
-        'license': 'MIT',
-        'main': '',
-        'style': '',
-        'files': [],
-        'dependencies': {},
-        'backend': {
-          'template': '',
-          'fixture': '',
-          'options': ''
+        license: "MIT",
+        main: "",
+        style: "",
+        files: [],
+        dependencies: {},
+        backend: {
+          template: "",
+          fixture: "",
+          options: ""
         }
       };
 
       if (this.props.addScripts) {
-        this.packageJson.main = this.props.name + '.js';
+        this.packageJson.main = `${this.props.name}.js`;
         this.packageJson.dependencies = {
-          'components/jquery': '*',
-          'components/jqueryui': '*'
+          "components/jquery": "*",
+          "components/jqueryui": "*"
         };
-        this.packageJson.files.push(this.props.name + '.js');
+        this.packageJson.files.push(`${this.props.name}.js`);
       }
       if (this.props.addStyles) {
-        this.packageJson.style = this.props.name + '.css';
-        this.packageJson.files.push(this.props.name + '.css');
+        this.packageJson.style = `${this.props.name}.css`;
+        this.packageJson.files.push(`${this.props.name}.css`);
       }
       if (this.props.addTemplate) {
         this.packageJson.files.push(this.props.name + this.props.templateName);
-        this.packageJson.backend.template = this.props.name + this.props.templateName;
+        this.packageJson.backend.template =
+          this.props.name + this.props.templateName;
       }
 
-      this.write('package.json', JSON.stringify(this.packageJson, null, 2));
+      this.write("package.json", JSON.stringify(this.packageJson, null, 2));
     },
 
-    projectFiles: function() {
-      var i;
-      var projectFiles = [
-        'editorconfig',
-        'jshintrc'
-      ];
+    projectFiles() {
+      let i;
+      const projectFiles = ["editorconfig", "jshintrc"];
       for (i = 0; i < projectFiles.length; i += 1) {
         this.fs.copy(
           this.templatePath(projectFiles[i]),
-          this.destinationPath('.' + projectFiles[i])
+          this.destinationPath(`.${projectFiles[i]}`)
         );
       }
     }
--- test/test-app.js
+++ test/test-app.js
@@ -1,24 +1,18 @@
-'use strict';
+const path = require("path");
+const assert = require("yeoman-generator").assert;
+const helpers = require("yeoman-generator").test;
+const os = require("os");
 
-var path = require('path');
-var assert = require('yeoman-generator').assert;
-var helpers = require('yeoman-generator').test;
-var os = require('os');
-
-describe('compost:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
+describe("compost:app", function() {
+  before(function(done) {
+    helpers
+      .run(path.join(__dirname, "../app"))
       .withOptions({ skipInstall: true })
       .withPrompts({ someOption: true })
-      .on('end', done);
+      .on("end", done);
   });
 
-  it('creates files', function () {
-    assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
-    ]);
+  it("creates files", function() {
+    assert.file(["bower.json", "package.json", ".editorconfig", ".jshintrc"]);
   });
 });
